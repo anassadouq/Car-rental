69609dc083e5c16a6c4218155b87a35e391db81a
anas sadouq <sadouqanas1@gmail.com>
2024-05-16 18:53:33 +0100

    export update

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -130,25 +130,25 @@
                             buttons: [{
                                 extend: 'excelHtml5',
                                 exportOptions: {
-                                    columns: [ 0,1,2,3,4,5 ]
+                                    columns: [ 0,1,2,3,4 ]
                                 }
                             },
                             {
                                 extend: 'pdfHtml5',
                                 exportOptions: {
-                                    columns: [ 0,1,2,3,4,5 ]
+                                    columns: [ 0,1,2,3,4 ]
                                 }
                             },
                             {
                                 extend: 'csvHtml5',
                                 exportOptions: {
-                                    columns: [ 0,1,2,3,4,5 ]
+                                    columns: [ 0,1,2,3,4 ]
                                 },
                             },
                             {
                                 extend: 'print',
                                 exportOptions: {
-                                    columns: [ 0,1,2,3,4,5 ]
+                                    columns: [ 0,1,2,3,4 ]
                                 }
                             }],
                         }]
