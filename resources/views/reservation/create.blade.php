9a9b732325f43c8f9d9a4af61835f718d0dad61e
anas sadouq <sadouqanas1@gmail.com>
2024-12-31 15:48:35 +0100

    ajouter un text dans select de reservation

--- resources/views/reservation/create.blade.php
+++ resources/views/reservation/create.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Réservation</title>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <style>
         label {
@@ -36,7 +37,7 @@
                     @csrf
                     <div class="mb-3">
                         <label class="form-label">Client :</label>
-                        <select name="id_client" class="my-3">
+                        <select name="id_client" class="my-3" id="client">
                             @foreach($clients as $client)
                                 <option value="{{ $client->id }}">{{ $client->prenom }} {{ $client->nom }}</option>
                             @endforeach
@@ -44,7 +45,7 @@
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Voiture :</label>
-                        <select name="id_voiture" class="my-3" id="text">
+                        <select name="id_voiture" class="my-3" id="voiture">
                             @foreach($voitures as $voiture)
                                 @if($voiture->reservee =="Non")
                                     <option value="{{ $voiture->id }}">{{ $voiture->marque }} {{ $voiture->matricule }}</option>
@@ -65,5 +66,23 @@
             </div>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#client').select2({
+                placeholder: 'Rechercher un article',
+                allowClear: true
+            });
+        });
+
+        $(document).ready(function() {
+            $('#voiture').select2({
+                placeholder: 'Rechercher un article',
+                allowClear: true
+            });
+        });
+    </script>
 </body>
 </html>
